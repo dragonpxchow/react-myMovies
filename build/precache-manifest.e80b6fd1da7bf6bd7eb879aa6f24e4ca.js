12d05998db778dc4f4f54168a4c6bc4065b7ddd6
Dragon Chow <dragonpxchow@gmail.com>
2020-05-26 01:33:30 +1000

    new build

---
+++ build/precache-manifest.e80b6fd1da7bf6bd7eb879aa6f24e4ca.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b513ddd21d0fbc7b748a511f86f71a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98050d455cc4c0a0aefb",
+    "url": "/static/css/2.019a5ac5.chunk.css"
+  },
+  {
+    "revision": "2f7bd9a5a4fbec5dd8dc",
+    "url": "/static/css/main.97130cc6.chunk.css"
+  },
+  {
+    "revision": "98050d455cc4c0a0aefb",
+    "url": "/static/js/2.3fb40d2f.chunk.js"
+  },
+  {
+    "revision": "eb01fbb835de944eb7ce9e9a91521c43",
+    "url": "/static/js/2.3fb40d2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f7bd9a5a4fbec5dd8dc",
+    "url": "/static/js/main.d6ecf722.chunk.js"
+  },
+  {
+    "revision": "fcc84ef30642b9d7c1e9",
+    "url": "/static/js/runtime-main.904bdf3e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
